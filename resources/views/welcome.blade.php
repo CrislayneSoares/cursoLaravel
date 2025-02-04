a18dd4c5b4897df149e2f9a026b6afcbab767dd6
CrislayneSoares <crissoaresr@hotmail.com>
2025-02-03 23:14:04 -0300

    add img and calender

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,6 @@
 @section('content')
 
 <h1>Página principal</h1>
+<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FBelem&showPrint=0&showTitle=0&showTz=0&showCalendars=0&src=Y3Jpc3NvYXJlczM0NkBnbWFpbC5jb20&color=%23039BE5" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
 @endsection
